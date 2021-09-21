b043637c703b807640b5833c206d7d64d00eca1f
GuiDah <guillaume-1982@hotmail.fr>
2021-09-21 16:41:52 +0200

    "Mise à jour du main.py"

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import csv
+import os
+
+import requests
+from bs4 import BeautifulSoup
+
+from constants import DATA_DIR
+
+url = 'http://books.toscrape.com/'
+response = requests.get(url)
+categorie_element = []
+if response.ok:
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    categories_elements = soup.find_all("div", class_="side_categories")
+
+    for categorie_element in categories_elements:
+        categorie_element = categorie_element.find('li')
+        print(categorie_element.text)
+
+
+
+
+
+
+
+
+
 
