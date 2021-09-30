3f02051a34ea2e2ccee9db93dee1983f6ae4183c
GuiDah <guillaume-1982@hotmail.fr>
2021-09-30 09:12:57 +0200

    "mise à jour du module book.py"

--- book.py
+++ book.py
@@ -1,3 +1,6 @@
+import csv
+import os
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -20,18 +23,22 @@ def book(url):
 
     if response.ok:
         soup = BeautifulSoup(response.text, 'html.parser')
+        book_informations["product_page_url"] = url
         book_informations["titre"] = soup.find("h1").text
         book_informations["universal_product_code"] = soup.find_all("td")[0].text
         book_informations["price_excluding_tax"] = soup.find_all("td")[2].text.replace("Â", "")
         book_informations["price_including_tax"] = soup.find_all("td")[3].text.replace("Â", "")
         book_informations["product_description"] = soup.find("div", class_="sub-header").next_sibling.next_sibling.text
-        #book_informations["category"] = soup.find("ul", class_="breadcrumb").find("li").find("a")["href"]
-
+        book_informations["category"] = soup.find_all("li")[2].text.replace("\n", "")
         book_informations["number_available"] = soup.find_all("td")[6].text
-
+        star = ["star-rating One", "star-rating Two", "star-rating Three", "star-rating Four", "star-rating Five"]
+        book_informations["review_rating"] = soup.find("p", class_=star).get('class')[-1]
+        book_informations["number_available"] = soup.find("p", class_="instock availability").text.replace("\n", "").replace(" ","").replace("Instock(", "").replace("available)", "")
         return book_informations
     else:
         print("une erreur est survenue")
 
-infos = book(url = "http://books.toscrape.com/catalogue/soumission_998/index.html")
+
+infos = book(url="http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html")
 print(infos)
+
--- category.py
+++ category.py
@@ -16,6 +16,7 @@ def scrap_one_categories(url):
         liste_book = soup.find("ol", class_="row")
         find__book = liste_book.find_all("li")
 
+
         all_books = []
         for elements_livre in find__book:
             div_contenair = lien_html(elements_livre.find("div", class_="image_container").find("a")["href"])
@@ -25,22 +26,22 @@ def scrap_one_categories(url):
     else:
         print("une erreur est survenue")
 
-infos = scrap_one_categories(url="http://books.toscrape.com/catalogue/category/books/classics_6/index.html")
-#print(infos)
+""""infos = scrap_one_categories(url="http://books.toscrape.com/catalogue/category/books/classics_6/index.html")"""
+"""print(infos)"""
 
 def scrap_all_categories(url):
     """find all categories"""
     response = requests.get(url)
+
     if response.ok:
         soup = BeautifulSoup(response.text, 'html.parser')
-        liste_all_books = soup.find("div", class_="page_inner")
+        liste_all_books = soup.find("div", class_="side_categories")
         find_all_books = liste_all_books.find_all("li")
-        all_books_all_catgories = []
+
         for all_categories in find_all_books:
-            all_contenair = lien_html(all_categories.find("ul", class_="nav nav-list").find("a")["href"])
-            all_books_info = book(scrap_one_categories(all_contenair))
-            all_books_all_catgories.append(all_books_info)
-        return all_books_all_catgories
+            all_contenair = all_categories.find("a")["href"]
+            print(all_contenair)
+        return all_contenair
     else:
         print("une erreur est survenue lors scrap du site")
         
